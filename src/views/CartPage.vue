f1c7e8e91e043ef59d8f2c85932dd73927b5a57c
Shaun W <shaun.p.wassell@gmail.com>
2020-08-30 12:48:59 -0400

    01.05 End

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -9,7 +9,19 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-    color: green;
-}
+  h1 {
+    border-bottom: 1px solid black;
+    margin: 0;
+    margin-top: 16px;
+    padding: 16px;
+  }
+
+  #total-price {
+    padding: 16px;
+    text-align: right;
+  }
+
+  #checkout-button {
+    width: 100%;
+  }
 </style>
